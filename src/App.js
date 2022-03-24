4d99d74c860f640f7577d2534680e92cd90a9e1a
michaelballos <mdb3091207@gmail.com>
2022-03-24 14:26:49 -0500

    Style Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calc-grid">
+      <div className="display">
+        <div className="previousDisplay">3000</div>
+        <div className="currentDisplay"> 5,000 + 3</div>
+      </div>
+        <button className="grey">AC</button>
+        <button className="grey">+/-</button>
+        <button className="grey">%</button>
+        <button className="yellow">÷</button>
+        <button className="dark">7</button>
+        <button className="dark">8</button>
+        <button className="dark">9</button>
+        <button className="dark">X</button>
+        <button className="dark">4</button>
+        <button className="dark">5</button>
+        <button className="dark">6</button>
+        <button className="yellow">-</button>
+        <button className="dark">1</button>
+        <button className="num">2</button>
+        <button className="dark">3</button>
+        <button className="yellow">+</button>
+        <button className="spanTwo">0</button>
+        <button className="dark">.</button>
+        <button className="yellow">=</button>
     </div>
-  );
+  )
 }
 
 export default App;
