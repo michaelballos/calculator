5b3ada4e50ee95f850121b8095842b76c13c1a07
michaelballos <mdb3091207@gmail.com>
2022-03-25 00:44:06 -0500

    Computer Transfer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import './styles.css';
+import {useState} from 'react';
+
+
+
+
+
+
 
 function App() {
+const [currentDisplay, setDisplay] = useState(0);
+}
+
   return (
     <div className="calc-grid">
       <div className="display">
-        <div className="previousDisplay">3000</div>
-        <div className="currentDisplay"> 5,000 + 3</div>
+        <div className="previousDisplay"></div>
+        <div className="currentDisplay">{currentDisplay}</div>
       </div>
-        <button className="grey">AC</button>
+
+      <div id="specialInput">
+
+      </div>
+
+        <button className="grey" onClick="setDisplay()">AC</button>
         <button className="grey">+/-</button>
         <button className="grey">%</button>
         <button className="yellow">÷</button>
         <button className="dark">7</button>
         <button className="dark">8</button>
         <button className="dark">9</button>
-        <button className="dark">X</button>
+        <button className="yellow">*</button>
         <button className="dark">4</button>
         <button className="dark">5</button>
         <button className="dark">6</button>
         <button className="yellow">-</button>
         <button className="dark">1</button>
-        <button className="num">2</button>
+        <button className="dark">2</button>
         <button className="dark">3</button>
         <button className="yellow">+</button>
         <button className="spanTwo">0</button>
